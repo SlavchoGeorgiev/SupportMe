bce44e84c785fd12e02ce0f475f29a714ae55eba
Slavcho Georgiev <georgiev.slavcho@gmail.com>
2016-02-24 22:19:50 +0200

    Add cache for company dropdown.

--- Source/Web/SupportMe.Web/Areas/Admin/Controllers/CompanyController.cs
+++ Source/Web/SupportMe.Web/Areas/Admin/Controllers/CompanyController.cs
@@ -25,6 +25,7 @@
 
         [HttpGet]
         [ActionName("Read")]
+        [OutputCache(Duration = 3 * 60)]
         public ActionResult ReadGet([DataSourceRequest]DataSourceRequest request)
         {
             var companies = this.companyService
